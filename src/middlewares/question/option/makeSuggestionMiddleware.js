4207bd655e760695beb1917a11e042689e1f53c8
greenina <igreen0485@kaist.ac.kr>
2022-07-26 21:24:23 +0900

    Modify makeSuggestionMiddleware

--- src/middlewares/question/option/makeSuggestionMiddleware.js
+++ src/middlewares/question/option/makeSuggestionMiddleware.js
@@ -1,7 +1,17 @@
 const Option = require('../../../db/option')
+const {ObjectId} = require('mongodb')
 
 const makeSuggestionMiddleware = (req, res) => {
-    // 
+    const oid = req.body.oid
+    const newSuggestion = req.body.newSug
+    Option.updateOne({_id: ObjectId(oid)},{$push:{suggesetions:newSuggestion}},(err, data) => {
+        if(err) throw err;
+        else {
+            res.json({
+                success: true
+            })
+        }
+    })
 }
 
 module.exports = makeSuggestionMiddleware
