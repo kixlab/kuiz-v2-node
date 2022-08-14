2b8376a2d26cb8b6d062ce309901c38b44146c79
greenina <igreen0485@kaist.ac.kr>
2022-08-15 06:58:18 +0900

    Set starter for checkIsInClassMiddleware

---
+++ src/middlewares/auth/checkIsInClassMiddleware.js
@@ -0,0 +1,9 @@
+
+const { ObjectId } = require('mongodb');
+const User = require('../../db/user')
+const Class = require('../../db/class')
+const checkIsInClassMiddleware = (req, res) => {
+
+}
+
+module.exports = joinClassMiddleware;
