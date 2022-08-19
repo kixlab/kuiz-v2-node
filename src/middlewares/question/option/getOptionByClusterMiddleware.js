d25be5202a581b11f9dfe7b3085d06660625ce0f
greenina <igreen0485@kaist.ac.kr>
2022-08-20 00:58:38 +0900

    Create getOptionByClusterMiddleware

---
+++ src/middlewares/question/option/getOptionByClusterMiddleware.js
@@ -0,0 +1,23 @@
+const OptionCluster = require('../../../db/optionCluster')
+const Option = require('../../../db/option')
+const ObjectId = require('mongodb')
+
+const getOptionByClusterMiddleware = (req, res) => {
+    const clusterId = req.query.clusterId
+    OptionCluster.findById(ObjectId(clusterId),(err, data) => {
+        if(err) throw err;
+        else {
+            Option.find({_id:{$in:data.ansList.map((o) => ObjectId(o))}}, (err, data2) => {
+                Option.find({_id:{$in:data.disList.map((o)=> ObjectId(o))}}, (err, data3) => {
+                    res.json({
+                        success:true,
+                        ansList:data2,
+                        disList:data3
+                    })
+                })
+            })
+        }
+    })
+}
+
+module.exports = getOptionByClusterMiddleware
