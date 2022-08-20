1b0d9899f7650ce960317d1b9272d4b5e2d08fb5
greenina <igreen0485@kaist.ac.kr>
2022-08-21 02:34:26 +0900

    Resolve promise for getQstemByOptionMiddleware

--- src/middlewares/question/getQstemByOptionMiddleware.js
+++ src/middlewares/question/getQstemByOptionMiddleware.js
@@ -1,17 +1,17 @@
 var Qstem = require('../../db/qstem')
 var ObjectId = require('mongodb').ObjectId
 
-const getQstemByOptionMiddleware = (req, res) => {
+const getQstemByOptionMiddleware = async (req, res) => {
 
     const qstems = req.body.qstems.map(q => ObjectId(q))
-    Qstem.find({_id:{$in:qstems}},(err, data) => {
-        if(err) throw err;
-        else {
-            res.json({
-                qstems:data
-            })
-        }
+    const details  = await Promise.all(qstems.map(async qstem => {
+        return await Qstem.findById(qstem)
+    }))
+
+    res.json({
+        qstems:details
     })
+
 
 }
 
