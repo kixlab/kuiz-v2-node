e46e554373b8a23f959f387ca9d50116e3fd7ea6
jhw123 <hyoungwookjin123@gmail.com>
2022-11-07 20:45:55 +0900

    🔨 added a script to restart app

--- src/api/statusCheck.ts
+++ src/api/statusCheck.ts
@@ -2,4 +2,5 @@ export interface StatusCheckParams {}
 
 export interface StatusCheckResults {
   success: boolean
+  startTime: string
 }
--- src/controllers/statusCheck.ts
+++ src/controllers/statusCheck.ts
@@ -1,10 +1,13 @@
-import { setTimeout } from 'timers/promises'
+import { format } from 'date-fns'
 import { StatusCheckParams, StatusCheckResults } from '../api/statusCheck'
 import { Get } from './methods'
+import { utcToZonedTime } from 'date-fns-tz'
+
+const startTime = utcToZonedTime(Date.now(), 'Asia/Seoul')
 
 export const statusCheck = Get<StatusCheckParams, StatusCheckResults>(async () => {
-  await setTimeout(3000)
   return {
     success: true,
+    startTime: format(startTime, 'yyyy-MM-dd HH:mm:ss'),
   }
 })
